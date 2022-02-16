e8dc7b0f0703505e2f02c3314218ceb8273fb8c5
sdub18 <sdubois@umass.edu>
2022-02-16 01:16:46 -0500

    work on crud file

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATEBASE_URL = ""
+SQLALCHEMY_DATEBASE_URL = "postgresql://postgres:password@localhost:5432/rescue_db_dev"
 
 engine = create_engine(
     SQLALCHEMY_DATEBASE_URL,
