64e42e7facc4fcdc4004329543a3d6cbcb97d979
sdub18 <sdubois@umass.edu>
2022-02-15 22:04:32 -0500

    created alembic files

---
+++ alembic/versions/83c0a218cd0b_create_notes_table.py
@@ -0,0 +1,24 @@
+"""create notes table
+
+Revision ID: 83c0a218cd0b
+Revises: 
+Create Date: 2022-02-15 22:03:59.319110
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '83c0a218cd0b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
