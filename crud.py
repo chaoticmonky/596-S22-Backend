a02821b94ef480e52fdcef81b7efeeac6eeae175
sdub18 <sdubois@umass.edu>
2022-02-16 14:51:17 -0500

    created crud.py

---
+++ crud.py
@@ -0,0 +1,37 @@
+from statistics import mode
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+# Get User for User ID
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+# Get User for Email
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+# Get Users
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+# Create user with username and password
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notReallyHashed"
+    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+# Get all available items
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+# Create Item with User ID
+def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    db_item = models.Item(**item.dict(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
