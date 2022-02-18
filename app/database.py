0d6f8d989130a9058357fc8cbb96adbac34ee794
sdub18 <sdubois@umass.edu>
2022-02-18 18:41:44 -0500

    At a bit of stand still. Not sure how to fix the docker file so that postgres runs properly

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5500/test_server"
+DATABASE_URL = "postgresql://username:password@localhost:8005/default_database"
 
 engine = create_engine(
     DATABASE_URL
